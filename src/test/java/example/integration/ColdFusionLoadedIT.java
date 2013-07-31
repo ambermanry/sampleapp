608c51ddc39c65eaac2d8c11493f580e5ea82d11
amanry <amanry@korefederal.com>
2013-07-31 16:35:02 -0400

    fix build errors

--- src/test/java/example/integration/ColdFusionLoadedIT.java
+++ src/test/java/example/integration/ColdFusionLoadedIT.java
@@ -23,7 +23,7 @@ public class ColdFusionLoadedIT {
 	@Test
 	public void testColdFusionLoadedIT() throws Exception {
 		driver.get(baseUrl + "index.cfm");
-		assertEquals("Welcome to the Rail World!", driver.findElement(By.id("navPrimary")).getText());
+		assertEquals("Welcome to the Railo World!", driver.findElement(By.id("navPrimary")).getText());
 		//assertEquals("coldfusion", driver.findElement(By.xpath("//table/tbody/tr[2]/td")).getText());
 		//assertEquals("test","test"); 
 	}
